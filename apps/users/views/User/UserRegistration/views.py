c0f6bd2a33fc17940e5db4eab0b6ae9409b3c07e
Your NameShohjahon2707 <saidovshohjahon000@gmail.com>
2025-08-21 19:30:51 +0500

    yangiladim

--- apps/users/views/User/UserRegistration/views.py
+++ apps/users/views/User/UserRegistration/views.py
@@ -7,9 +7,26 @@ def user_register_view(request):
     if request.method == 'GET':
         return render(request, 'users/registration.html')
 
+    # Обязательные поля
     username = request.POST.get("username")
     password = request.POST.get("password")
     password2 = request.POST.get("password2")
+    first_name = request.POST.get("first_name")
+    last_name = request.POST.get("last_name")
+    phone_number = request.POST.get("phone_number")
+
+    # Необязательные поля
+    gender = request.POST.get("gender")
+    shift = request.POST.get("shift")
+    study_lang = request.POST.get("study_lang")
+    address = request.POST.get("address")
+    blood_group = request.POST.get("blood_group")
+    bio = request.POST.get("bio")
+
+    # Проверка обязательных полей
+    if not all([username, password, password2, first_name, last_name, phone_number]):
+        messages.error(request, 'Iltimos, barcha majburiy maydonlarni to‘ldiring.')
+        return redirect('register')
 
     if User.objects.filter(username=username).exists():
         messages.error(request, 'Bunday username allaqachon mavjud.')
@@ -19,7 +36,21 @@ def user_register_view(request):
         messages.error(request, 'Parollar mos emas.')
         return redirect('register')
 
-    user = User.objects.create_user(username=username, password=password)
-    login(request, user)  # Автоматический вход
+    # Создание пользователя
+    user = User.objects.create_user(
+        username=username,
+        password=password,
+        first_name=first_name,
+        last_name=last_name,
+        phone_number=phone_number,
+        gender=gender,
+        shift=shift,
+        study_lang=study_lang,
+        address=address,
+        blood_group=blood_group,
+        bio=bio
+    )
+
+    login(request, user)
     messages.success(request, "Ro'yxatdan o'tdingiz va tizimga kirdingiz!")
-    return redirect('home')
+    return redirect('login')
